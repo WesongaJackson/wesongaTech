f9c5709dfdbe0ec2ff9b310d3f780282a362af6d
WesongaJackson <hackingson2@gmail.com>
2024-06-16 19:11:08 +0300

    updated site

--- firewall.sh
+++ firewall.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-#!/bin/sh
-sudo apt-get -y update
-sudo apt-get -y install ufw
-sudo ufw disable
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo systemctl stop ufw
-sudo ufw allow 22/tcp
-sudo ufw allow 443/tcp
-sudo ufw allow 80/tcp
-sudo ufw enable
-#!/bin/bash
 set -e
 
 # Ensure the system is updated and install necessary packages
@@ -24,7 +12,9 @@ sudo ufw default allow outgoing
 sudo ufw allow 22/tcp   # Allow SSH
 sudo ufw allow 443/tcp  # Allow HTTPS
 sudo ufw allow 80/tcp   # Allow HTTP
-sudo ufw enable         # Enable ufw with the new rules
+
+# Enable ufw with the new rules, automatically confirming the prompt
+yes | sudo ufw enable
 
 # Restart ufw to apply changes (this is optional)
 sudo systemctl restart ufw
